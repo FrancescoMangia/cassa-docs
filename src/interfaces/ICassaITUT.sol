247c2771cbac4496a89875145f24eaa4ae736ae1
lucas <15702827+lucasgleba@users.noreply.github.com>
2026-01-08 17:32:44 +0800

    feat(ICassaITUT): settlementRatio

--- src/interfaces/ICassaITUT.sol
+++ src/interfaces/ICassaITUT.sol
@@ -117,4 +117,9 @@ interface ICassaITUT {
 
     /// @notice Settles the policy and fixes the settlement ratio
     function settle() external;
+
+    /// @notice Returns the settlement ratio and whether the policy has been settled
+    /// @return ratio The settlement ratio value scaled by 1e18 (1e18 = 100%)
+    /// @return settled Whether the policy has been settled
+    function settlementRatio() external view returns (uint256 ratio, bool settled);
 }
