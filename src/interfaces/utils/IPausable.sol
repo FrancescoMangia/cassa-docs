050d168f1ddc5a64724e21e5957a460d04c632fd
lucas <15702827+lucasgleba@users.noreply.github.com>
2026-01-15 17:59:12 +0700

    feat: utils

---
+++ src/interfaces/utils/IPausable.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IPausable {
+    /// @notice Returns true if the contract is paused, and false otherwise
+    /// @return _paused True if paused, false otherwise
+    function paused() external returns (bool _paused);
+
+    /// @notice Pause the contract, revert if already paused
+    function pause() external;
+
+    /// @notice Unpause the contract, revert if not paused
+    function unpause() external;
+}
